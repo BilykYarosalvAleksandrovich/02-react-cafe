8cb870f75cede0d0c6d7eb7beac9463660d4e366
BilykYarosalvAleksandrovich <bilyk.yaroslav.aleksandrovich@gmail.com>
2025-11-03 17:21:08 +0200

    for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/02-react-cafe/",
+  base: "/",
   build: {
     sourcemap: true,
   },
